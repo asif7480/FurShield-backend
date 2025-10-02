fd70989ef192ddf96f490a91e13245e8ffe1067b
asif7480 <a.a.khan7480@gmail.com>
2025-10-02 18:11:40 +0500

    Fix: change samesite from strict to none

--- controllers/auth.controller.mjs
+++ controllers/auth.controller.mjs
@@ -158,7 +158,7 @@ export const login = asyncHandler(async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true, // JS can't access
     secure: process.env.NODE_ENV === "production", // only HTTPS in prod
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000, // 1 day
   });
 
